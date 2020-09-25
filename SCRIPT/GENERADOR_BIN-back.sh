2338ee180529ac4d534d9d7652ab723a2075c567
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-09-25 15:04:02 -0500

    Update GENERADOR_BIN-back.sh

--- SCRIPT/GENERADOR_BIN-back.sh
+++ SCRIPT/GENERADOR_BIN-back.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Bin _ Gen #OFC
-link_bin="https://www.dropbox.com/s/37psc5yx7060y22/generadorcc.py?dl=0"
-[[ ! -e /usr/bin/generadorcc.py ]] && wget -O /usr/bin/generadorcc.py ${link_bin} > /dev/null && chmod +x /usr/bin/generadorcc.py
-msg -ama "$(fun_trans "GERADOR DE BINS OFICIAL")"
+link_bin="https://raw.githubusercontent.com/ThonyDroidYT/ADM-Ultimate-Plus/master/Install/ADM-CCGen+.py"
+[[ ! -e /usr/bin/ADM-CCGen+.py ]] && wget -O /usr/bin/ADM-CCGen+.py ${link_bin} > /dev/null && chmod +x /usr/bin/ADM-CCGen+.py
+msg -ama "$(fun_trans "GERADOR DE BINS ADM+ OFICIAL")"
 msg -bar
-msg -ne "$(fun_trans "Digite a bin"): " && read UsrBin
+msg -ne "$(fun_trans "Digite un bin"): 》" && read UsrBin
 while [[ ${#UsrBin} -lt 16 ]]; do
 UsrBin+="x"
 done
-msg -ne "$(fun_trans "Quantas Bins Quer Gerar"): " && read GerBin
+msg -ne "$(fun_trans "Cuantos Bins Quieres Generar"): 》" && read GerBin
 [[ $GerBin != +([0-9]) ]] && GerBin=10
 [[ -z $GerBin ]] && GerBin=10
 msg -bar
-python /usr/bin/generadorcc.py -b ${UsrBin} -u ${GerBin} -d -c
+python /usr/bin/ADM-CCGen+.py -b ${UsrBin} -u ${GerBin} -d -c
 msg -bar
