c725c44e4b0a4a56500316db11900e26a9f0c2b3
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-09-27 14:50:51 -0500

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -42,7 +42,7 @@ apt-get install figlet -y
 apt-get install cowsay -y
 apt-get install lolcat -y
 echo -e "\033[1;35m======================================================\033[0m"
-echo -e "\033[1;32mPreparando Servidor... "
+echo -e "\033[1;31mPreparando Servidor... "
 echo -e "\033[1;35m======================================================\033[0m"
 meu_ip () {
 MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
@@ -59,7 +59,7 @@ wget -O "$HOME/lista-arq" ${REQUEST}/GERADOR > /dev/null 2>&1
 sleep 1s
 [[ -e $HOME/lista-arq ]] && {
 for arqx in `cat $HOME/lista-arq`; do
-echo -ne "\033[1;32mDescargando Archivos \033[1;31m[$arqx] "
+echo -ne "\033[1;32mDescargando Archivos \033[1;35m[$arqx] "
 wget -O $HOME/$arqx ${REQUEST}/${arqx} > /dev/null 2>&1 && {
 echo -e "\033[1;35m- \033[1;32mRecebido Con Exito!"
 [[ -e $HOME/$arqx ]] && veryfy_fun $arqx
