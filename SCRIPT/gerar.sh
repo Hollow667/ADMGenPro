602417c43ea49b364ea6fc62b3ad0f1939a2a3db
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-10-03 14:28:37 -0500

    Update gerar.sh

--- SCRIPT/gerar.sh
+++ SCRIPT/gerar.sh
@@ -3,7 +3,7 @@
 clear
 [[ -e /etc/newadm-instalacao ]] && BASICINST="$(cat /etc/newadm-instalacao)" || BASICINST="menu PGet.py ports.sh ADMbot.sh message.txt usercodes sockspy.sh POpen.py PPriv.py PPub.py PDirect.py speedtest.py speed.sh utils.sh dropbear.sh apacheon.sh openvpn.sh shadowsocks.sh ssl.sh squid.sh Crear-Demo.sh version.txt monitor.sh estilos.css"
 IVAR="/etc/http-instas"
-BARRA="\033[0;34m======================================================\033[0m"
+BARRA="\033[0;34m========================================================\033[0m"
 echo -e "$BARRA"
 figlet ADMGenPlus | lolcat
 echo -e "$BARRA"
