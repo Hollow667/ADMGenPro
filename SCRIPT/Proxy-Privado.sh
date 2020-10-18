7dad7003cee82eccdf468af93c995119f9efd4af
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-10-18 09:10:59 -0500

    Update Proxy-Privado.sh

--- SCRIPT/Proxy-Privado.sh
+++ SCRIPT/Proxy-Privado.sh
@@ -22,7 +22,7 @@ echo -e " \033[1;33m[\033[1;31m####################\033[1;33m] - \033[1;32m100%\
 sleep 1s
 }
 clear
-cowsay -f eyes "Esta herramienta le cambia y da color al status de conexion y agrega una contrasena atu payload para mayor seguridad...." | lolcat 
+cowsay -f eyes "Esta herramienta le cambia y da color al status de conexion y agrega una contrasena a tu payload para mayor seguridad...." | lolcat 
 figlet ..ThonyDroid.. | lolcat
 BARRA="\e[1;34m========================================================\e[0m"
 echo -e "\e[1;34m========================================================\e[0m"
@@ -54,13 +54,13 @@ read mensage
 echo -e "$BARRA"
 echo -e "Seleccione El Color De Mensaje: "
 cat << Eof
-[ 1 ] -- #ff183f -- \033[1;31mrojo
-[ 2 ] -- #3fff18 -- \033[1;32mverde
-[ 3 ] -- #6518ff -- \033[1;35morado
-[ 4 ] -- #ff6518 -- naranja
-[ 5 ] -- #18ffd9 -- \033[1;36cyan
-[ 6 ] -- #ffd700 -- amarillo
-[ 7 ] -- #1e90ff -- \033[1;34mazul
+[ 1 ] -- #ff183f -- echo -e"\033[1;31mrojo"
+[ 2 ] -- #3fff18 -- echo -e"\033[1;32mverde"
+[ 3 ] -- #6518ff -- echo -e"\033[1;35morado"
+[ 4 ] -- #ff6518 -- echo -e"naranja"
+[ 5 ] -- #18ffd9 -- echo -e"\033[1;36cyan"
+[ 6 ] -- #ffd700 -- echo -e"amarillo"
+[ 7 ] -- #1e90ff -- echo -e"\033[1;34mazul"
 [ 8 ] -- #000000 -- negro
 [ 9 ] -- #00ff7f -- agua marina
 [ 10 ] -- #8b4513 -- cafe
